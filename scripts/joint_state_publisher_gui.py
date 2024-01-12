7edeff90747c42360285813d5ee37b767d5e51c9
rzeta <cs22b1093@iiitdm.ac.in>
2024-01-12 22:56:26 +0530

    Fixed Joints and Velocity sliders

--- scripts/joint_state_publisher_gui.py
+++ scripts/joint_state_publisher_gui.py
@@ -6,10 +6,10 @@ from sensor_msgs.msg import JointState
 
 class JointStatePublisherGUI:
     def __init__(self):
-        self.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5']
+        self.joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
         self.initial_joint_positions = [0.0] * len(self.joint_names)
         self.joint_positions = self.initial_joint_positions.copy()
-        self.joint_velocities = [0.0] * (len(self.joint_names) - 1)  # Velocity only for joint1 to joint4
+        self.joint_velocities = [0.0] * (len(self.joint_names) - 2)  # Velocity only for joint1 to joint4
         rospy.init_node('joint_state_publisher_gui')
 
         self.publisher = rospy.Publisher('/joint_states', JointState, queue_size=10)
@@ -28,7 +28,7 @@ class JointStatePublisherGUI:
         velocity_label.grid(row=0, column=2, padx=10, pady=5)
 
         self.position_scale_vars = [tk.DoubleVar(value=0.0) for _ in range(len(self.joint_names))]
-        self.velocity_scale_vars = [tk.DoubleVar(value=0.0) for _ in range(len(self.joint_names) - 1)]
+        self.velocity_scale_vars = [tk.DoubleVar(value=0.0) for _ in range(len(self.joint_names) - 2)]
 
         self.position_scales = []
         self.velocity_scales = []
@@ -41,7 +41,7 @@ class JointStatePublisherGUI:
             position_scale.grid(row=i + 1, column=1, padx=10, pady=5)
 
             # Create velocity scales only for joint1 to joint4
-            if i < len(self.joint_names) - 1:
+            if i < len(self.joint_names) - 2:
                 velocity_scale = tk.Scale(self.root, from_=-10.0, to=10.0, resolution=0.01, variable=self.velocity_scale_vars[i], orient=tk.HORIZONTAL)
                 velocity_scale.grid(row=i + 1, column=2, padx=10, pady=5)
                 self.velocity_scales.append(velocity_scale)
@@ -77,7 +77,7 @@ class JointStatePublisherGUI:
     def reset_joint_states(self):
         for i in range(len(self.joint_names)):
             self.position_scale_vars[i].set(self.initial_joint_positions[i])
-            if i < len(self.joint_names) - 1:
+            if i < len(self.joint_names) - 2:
                 self.velocity_scale_vars[i].set(0.0)
 
     def update_values(self, event):
@@ -89,7 +89,7 @@ class JointStatePublisherGUI:
                 self.position_scales[index].focus_set()
                 self.position_scales[index].set(float(event.char))
                 # Only set the velocity scale if it exists (joint1 to joint4)
-                if index < len(self.joint_names) - 1:
+                if index < len(self.joint_names) - 2:
                     self.velocity_scales[index].focus_set()
                     self.velocity_scales[index].set(float(event.char))
 
